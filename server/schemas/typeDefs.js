5a0c7d1a10e976cecbfa2d7bc0594089961a3534
Chau Pham <ncp9988@gmail.com>
2022-03-07 14:23:04 -0600

    abke to look up thoughts by username

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,17 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
   }
+  type Query {
+    thoughts(username: String): [Thought]
+  }
+  
 `;
 
 
