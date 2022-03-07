d87dee3dacebac387901ff952b1577b8ac5f7504
Chau Pham <ncp9988@gmail.com>
2022-03-07 14:59:26 -0600

    set up grapQl queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,29 @@ const resolvers = {
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
-          },
-          
+        },
+        thoughts: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+
     }
-  };
-  
+};
+
 
 module.exports = resolvers;
